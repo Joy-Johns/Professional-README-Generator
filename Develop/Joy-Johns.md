0e40fd63baafc2dfc30003adb19395ab48a8e30a
Joy Johns <JRJ20716@gmail.com>
2023-01-30 17:30:37 -0500

    Markdown updated w/ Badge

---
+++ Develop/Joy-Johns.md
@@ -0,0 +1,41 @@
+## I'm Almost Finished
+
+  [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://mit-license.org/)
+
+  ## Description
+  This is homework
+
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributors](#contributors)
+  * [Tests](#tests)
+  * [Credits](#credits)
+  
+  ## Installation
+  npm blaaa
+
+  ## usage
+  Ask e for permission 1st
+
+  ## Licenses
+    This project is covered under the MIT license. To learn more about what this means, click the license button at the top.
+
+  ## Contributors
+  Add all you want
+
+  ## Test
+  mpn blaaaa
+
+  ## Name
+  https://github.com/Joy-Johns
+
+  ## Email
+  Joy@joy.com
+
+
+  ## Questions
+  If you have an questions about hte repo, open an issue or contact me directly at 
+  Joy@joy.com. You can find more of my work at https://github.com/Joy-Johns.
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,38 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Returns the license link
+function renderLicenseLink(license) {
+  if (license.includes('MIT'))
+  {
+    return `https://mit-license.org/`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+  }
+}
 // TODO: Create a function to generate markdown for README
 
 function generateMarkdown(data) {
-  console.log(data);
-  console.log(data.username);
-  data.username = "Your name is " + data.username;
-  return `# ${data.title}
+  data.username = "https://github.com/" + data.username;
+  return `## ${data.title}
+
+  ${renderLicenseBadge(data.licenses)}
+
+  ## Description
+  ${data.description}
+
+
   ## Table of Contents
-  * [Description](#description)
   * [Installation](#installation)
   * [Usage](#usage)
   * [Licenses](#licenses)
-  * [Contributing](#contributing)
+  * [Contributors](#contributors)
   * [Tests](#tests)
-  * [Questions](#questions)
   * [Credits](#credits)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.licenses)}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Test
+  ${data.test}
+
   ## Name
   ${data.username}
+
   ## Email
   ${data.email}
-  ## Badge
-  ${data.badge}
-  
+
+
+  ## Questions
+  If you have an questions about hte repo, open an issue or contact me directly at 
+  ${data.email}. You can find more of my work at ${data.username}.
 `;
 }
 
