95676f0b9aebf52a8e05f4cc316ae3369d764daf
Joy Johns <JRJ20716@gmail.com>
2023-01-29 19:08:10 -0500

    Created function to write README file

--- Develop/index.js
+++ Develop/index.js
@@ -9,7 +9,7 @@ const questions = [
     {type:"input", name:"email", message:"What is your email address? "},
     {type:"input", name:"title", message:"What is your project's name? "},
     {type:"input", name:"description", message:"Please write a short description of your project. "},
-    {type:"input", name:"license", message:"What kind of license should your project have? "},
+    {type:"input", name:"license", message:"What kind of license should your project have? ", choices:["MIT"]},
     {type:"input", name:"installation", message:"What command should be run to install dependencies? "},
     {type:"input", name:"test", message:"What command should be run to run tests? "},
     {type:"input", name:"usage", message:"What does the user need to know about using the repo? "},
@@ -19,10 +19,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = data => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.md`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), 
+        (err) =>
+        err ? console.log(err) 
+        : console.log('Success!')  
+);}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
